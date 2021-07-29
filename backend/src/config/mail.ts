5dc3944e63c5a8f5d30275222ed21a41ba7227ae
rodrigodh <bombomdefrango1@gmail.com>
2021-07-29 13:21:09 -0300

    backend: AWS ses.

---
+++ backend/src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'rodrigoschieck.pro@gmail.com',
+      name: 'Rodrigo o Dev',
+    },
+  },
+} as IMailConfig;
