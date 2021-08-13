130318018a48085f6d1a4d25927902794a7cb34b
rodrigodh <bombomdefrango1@gmail.com>
2021-08-13 16:48:02 -0300

    frontend: Reset password.

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 
 import Route from './Route';
+
 import SigIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 
 import Dashboard from '../pages/Dashboard';
 
@@ -11,6 +14,8 @@ const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SigIn} />
     <Route path="/signup" exact component={SignUp} />
+    <Route path="/forgot-password" exact component={ForgotPassword} />
+    <Route path="/reset_password" exact component={ResetPassword} />
 
     <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
